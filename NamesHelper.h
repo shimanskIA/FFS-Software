a45dd7daf7604199303abb3f4f96241e866d284e
Ivan Shimanski <ivan_shi@list.ru>
2021-12-04 23:39:17 +0300

    modified solution architecture to make application distributable

--- NamesHelper.h
+++ NamesHelper.h
@@ -2,14 +2,14 @@
 
 #include <QString>
 
-const QString sampleStatePath = "samplestate.txt";
-const QString measurementStatePath = "measurementstate.txt";
-const QString equipmentStatePath = "equipmentstate.txt";
-const QString measurementParametersStatePath = "measurementparameterstate.txt";
-const QString equipmentParameterStatePath = "equipmentparameterstate.txt";
-const QString characteristicStatePath = "characteristicstate.txt";
-const QString characteristicTypeStatePath = "characteristictypestate.txt";
-const QString measuringSystemStatePath = "measuringsystemstate.txt";
+const QString sampleStatePath = "management/samplestate.txt";
+const QString measurementStatePath = "management/measurementstate.txt";
+const QString equipmentStatePath = "management/equipmentstate.txt";
+const QString measurementParametersStatePath = "management/measurementparameterstate.txt";
+const QString equipmentParameterStatePath = "management/equipmentparameterstate.txt";
+const QString characteristicStatePath = "management/characteristicstate.txt";
+const QString characteristicTypeStatePath = "management/characteristictypestate.txt";
+const QString measuringSystemStatePath = "management/measuringsystemstate.txt";
 
 const QString dbCharacteristics = "characteristics";
 const QString dbMeasurements = "measurements";
